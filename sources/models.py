1dd3a261c5181de84689400bdf9c8344c3a9eb5e
Andrii Porokhnavets <andrij843@gmail.com>
2018-04-21 12:39:56 +0300

    Add model Source

--- sources/models.py
+++ sources/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=255,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
