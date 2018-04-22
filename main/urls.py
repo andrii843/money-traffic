e3b79a5970da187e6b9a3df06d6fe37fd0158307
orestber <orestber@gmail.com>
2018-04-23 01:38:17 +0300

    djanro json response data

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,15 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('register/', views.RegisterFormView.as_view(), name='register_in'),
-    path('login/', views.LoginFormView.as_view(), name='login_in'),
+    # path('register/', views.RegisterFormView.as_view(), name='register_in'),
+    # path('login/', views.LoginFormView.as_view(), name='login_in'),
+
     path('user/', views.user_detail, name='user_detail'),
     path('profile/', views.update_profile, name='update_profile'),
+
+    
+    path('api/login/', views.login_user, name='login_in'),
+    path('api/logout/', views.logout_user, name='logout_user'),
+    path('api/user/', views.json_user_info, name='json_user_detail'),
+    path('api/register/', views.register_user, name='register_user'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,14 +5,19 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import Profile
 from .forms import ProfileForm
 from .forms import UserForm
-from django.contrib.auth import login
-from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from users.models import User
+
+
+import json
+
 
 # Create your views here.
 def main_page(request):
@@ -22,6 +27,52 @@ def main_page(request):
 def user_detail(request):
     user = request.user
     return render(request, 'users/user_detail.html', {'user': user})
+
+def json_user_info(request):
+    return JsonResponse({'is_auth':request.user.is_authenticated, 'username':request.user.username}, safe=False)
+
+
+def login_user(request):
+    ''' Проводимо авторизацію користувача на сайті '''
+    if request.method == 'POST':
+        body = request.body.decode("utf-8")
+        request.POST = json.loads(body)
+
+        username = request.POST['login']
+        passwd = request.POST['password']
+
+        user = authenticate(request, username=username, password=passwd)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"user":request.user.id},safe=False)
+    return JsonResponse({},safe=False)
+
+
+def logout_user(request):
+    ''' Вихід з профілю корисувача '''
+    logout(request)
+    return JsonResponse({},safe=False)
+
+def register_user(request):
+    if request.method == 'POST':
+        body = request.body.decode("utf-8")
+        request.POST = json.loads(body)
+
+        username = request.POST['login']
+        passwd = request.POST['password']
+        email = request.POST['email']
+
+        user = User.objects.create_user(username=username, email=email, password=passwd)
+        user.save()
+
+        ''' Після реєстрації проводимо авторизацію користувача '''
+        # user = authenticate(request, username=username, password=passwd)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"user":request.user.id},safe=False)        
+
+
+    return JsonResponse({},safe=False)
 
 
 class RegisterFormView(FormView):
